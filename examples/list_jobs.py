e9592931081739461ba756f4d35a4c94024a7fdc
creynolds15 <colin.reynolds@tamr.com>
2024-04-24 08:31:10 -0700

    chore: get example to work

--- examples/list_jobs.py
+++ examples/list_jobs.py
@@ -1,5 +1,19 @@
 from tamr_sdk.api_client import TamrApiClient
+import time
 
-tamr_client = TamrApiClient('svc-api.tamrapi.dev:443')
+start = time.time()
+tamr_client = TamrApiClient('<host-name>', [('x-api-key', '<api-key>')])
+one = time.time()
 
-tamr_client.jobs().list_jobs()
+
+jobs = tamr_client.jobs()
+two = time.time()
+
+response = jobs.list_jobs()
+three = time.time()
+
+response = jobs.get_job(job_id='<job-id>')
+four = time.time()
+
+print(response)
+print(f"APIClient took {one -start}; JobsClient took {two-one} and api call took {three-two} and second is {four-three}")
--- tamr_sdk/jobs/jobs_client.py
+++ tamr_sdk/jobs/jobs_client.py
@@ -4,25 +4,27 @@ from tamr.api.v1beta1.jobs_pb2_grpc import JobsStub
 from tamr.api.v1beta1 import jobs_pb2 as jobs
 
 class JobsClient:
-    def __init__(self, host):
-        channel = grpc.insecure_channel(host)
+    def __init__(self, host, metadata):
+        credentials = grpc.ssl_channel_credentials()
+        channel = grpc.secure_channel(host, credentials)
+        self.metadata = metadata
         self.stub = JobsStub(channel)
 
     def get_job(self, job_id):
-        request = jobs.GetJobRequest(jobId=job_id)
-        return self.stub.GetJob(request)
+        request = jobs.GetJobRequest(job_id=job_id)
+        return self.stub.GetJob(request, metadata=self.metadata)
 
     def list_jobs(self, page_size=10, page_token=None):
         request = jobs.ListJobsRequest(
             page_size=page_size,
             page_token=page_token
         )
-        return self.stub.ListJobs(request)
+        return self.stub.ListJobs(request, metadata=self.metadata)
 
     def stop_job(self, job_id):
-        request = jobs.StopJobRequest(jobId=job_id)
-        return self.stub.StopJob(request)
+        request = jobs.StopJobRequest(job_id=job_id)
+        return self.stub.StopJob(request, metadata=self.metadata)
 
     def create_job(self, job_definition):
         request = jobs.CreateJobRequest(job=job_definition)
-        return self.stub.CreateJob(request)
+        return self.stub.CreateJob(request, metadata=self.metadata)
