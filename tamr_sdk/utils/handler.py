5885d218de62ba5e0faee63b53d48fd68e676997
creynolds <63250905+creynolds15@users.noreply.github.com>
2024-05-07 07:06:11 -0700

    chore: Add in type hinting
    Co-authored-by: ErinCompaan <86741767+ErinCompaan@users.noreply.github.com>

--- tamr_sdk/utils/handler.py
+++ tamr_sdk/utils/handler.py
@@ -1,16 +1,30 @@
-import grpc
+from typing import Callable, TypeVar
 
-def exception_handler(func):
-    def handle_grpc_status_codes(*args, **kwargs):
+import grpc
+from typing_extensions import Concatenate, ParamSpec
+
+SelfT = TypeVar("SelfT")
+ArgT = ParamSpec("ArgT")
+ReturnT = TypeVar("ReturnT")
+
+
+def exception_handler(
+    func: Callable[Concatenate[SelfT, ArgT], ReturnT]
+) -> Callable[Concatenate[SelfT, ArgT], ReturnT]:
+    def handle_grpc_status_codes(
+        _self: SelfT, *args: ArgT.args, **kwargs: ArgT.kwargs
+    ) -> ReturnT:
         try:
-            func(*args, **kwargs)
+            func(_self, *args, **kwargs)
         except grpc._channel._InactiveRpcError as e:
             if e.code() == grpc.StatusCode.NOT_FOUND:
                 raise ValueError(e.details())
             elif e.code() == grpc.StatusCode.UNAVAILABLE:
                 raise ConnectionError(e.details())
             elif e.code() == grpc.StatusCode.UNAUTHENTICATED:
-                raise ConnectionRefusedError("Error connecting to client. Authentication is not valid")
+                raise ConnectionRefusedError(
+                    "Error connecting to client. Authentication is not valid"
+                )
             else:
                 raise e
 
